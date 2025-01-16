811f2d1355f3fb52695a4ed3b6a106f394ecb1c0
Clauda Yokomoi <claudayokomoi@gmail.com>
2025-01-16 14:15:24 +0300

    Updated project files

--- alx-project-2/components/layout/Header.tsx
+++ alx-project-2/components/layout/Header.tsx
@@ -1,12 +1,11 @@
 // components/layout/Header.tsx
 
 const Header = () => {
-    return (
-      <header className="bg-blue-600 text-white p-4">
-        <h1 className="text-xl">Welcome to ALX Project 2</h1>
-      </header>
-    );
-  };
-  
-  export default Header;
-  
+  return (
+    <header className="bg-blue-600 text-white p-4">
+      <h1 className="text-xl">Welcome to ALX Project 2</h1>
+    </header>
+  );
+};
+
+export default Header;
