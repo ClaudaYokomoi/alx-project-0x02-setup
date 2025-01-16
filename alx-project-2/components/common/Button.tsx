e98955cc19f140bb3ba30962ce1c9eef27b4c704
Clauda Yokomoi <claudayokomoi@gmail.com>
2025-01-16 15:30:42 +0300

    Create Button component with size and shape props

---
+++ alx-project-2/components/common/Button.tsx
@@ -0,0 +1,30 @@
+// components/common/Button.tsx
+
+import React from 'react';
+import { ButtonProps } from '../../interfaces';
+
+const Button: React.FC<ButtonProps> = ({ size, shape, onClick, children }) => {
+  // Define className based on size and shape props
+  const sizeClasses = {
+    small: 'px-4 py-2 text-sm',
+    medium: 'px-6 py-3 text-base',
+    large: 'px-8 py-4 text-lg',
+  };
+
+  const shapeClasses = {
+    'rounded-sm': 'rounded-sm',
+    'rounded-md': 'rounded-md',
+    'rounded-full': 'rounded-full',
+  };
+
+  return (
+    <button
+      className={`bg-blue-500 text-white ${sizeClasses[size]} ${shapeClasses[shape]} hover:bg-blue-600`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-2/pages/about.tsx
+++ alx-project-2/pages/about.tsx
@@ -1,11 +1,17 @@
 // pages/about.tsx
 
 import React from 'react';
+import Button from '../components/common/Button';
 
-const About = () => {
+const About: React.FC = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Us</h1>
+      <div>
+        <Button size="small" shape="rounded-sm">Small Button</Button>
+        <Button size="medium" shape="rounded-md">Medium Button</Button>
+        <Button size="large" shape="rounded-full">Large Button</Button>
+      </div>
     </div>
   );
 };
